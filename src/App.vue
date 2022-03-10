c8abe4bf92570643dbc37215c9838cbb3cdf94cc
Tavis <71576767+TavisMac1@users.noreply.github.com>
2022-03-10 17:25:59 -0400

    initial, data binded inputs, array push on form submission, null value on message checks

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1> {{test}} </h1>
+  <form @submit.prevent="SendMessage">
+    <h4> {{spontaneousAlert}} </h4>
+    <input type="text" name="" id="" @click="ShowAlert" v-model="message">
+    <input type="submit" value="send" @submit="SendMessage">
+  </form>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+ data() {
+   return {
+      spontaneousAlert: "",
+      message: 'Hello',
+      messageBank: []
+   }
+ },
+ methods: {
+   ShowAlert() {
+     this.spontaneousAlert = "Type a message and press submit to send it"
+   },
+   ShowText() {
+        this.test = ""
+      },
+   SendMessage() {
+        if (this.test != "" || this.test != null) {
+          this.messageBank.push(this.test)
+          console.log("Message added to array successfully");
+          console.log(this.messageBank[0])
+        }
+      },
+ }
 }
 </script>
 
